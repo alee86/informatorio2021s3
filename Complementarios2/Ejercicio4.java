7e49022c840a75025dce522fddc89a47d4ea8442
Alejandro <alejandroeguiazabal@gmail.com>
2021-10-08 17:25:06 -0300

    Complementarios 2 actualizado hasta el 4

---
+++ Complementarios2/Ejercicio4.java
@@ -0,0 +1,18 @@
+package Complementarios2;
+/*
+Cargar un arrayList con 12 nombres de estudiantes (String), luego separarlos en 3
+cursos (3 arrayList) e imprimir dichos cursos.
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList estudiantes = new ArrayList<String>();
+        ArrayList curso1 = new ArrayList();
+        ArrayList curso2 = new ArrayList();
+        ArrayList curso3 = new ArrayList();
+    }
+}
---
+++ Complementarios2/Ejercicio5.java
@@ -0,0 +1,8 @@
+package Complementarios2;
+/*
+Dados 2 ArrayList que contienen horas-trabajadas (array1) y valor-por-hora(array2)
+el resumen de carga de horas semanal de un empleado. Se debe generar otra lista que
+contenga los totales y luego imprimir el total final a cobrar.
+ */
+public class Ejercicio5 {
+}
