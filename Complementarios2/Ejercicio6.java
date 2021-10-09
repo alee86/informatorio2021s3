f2988800ac2c766ad19656b5f8aada4b27e3f5c2
Alejandro <alejandroeguiazabal@gmail.com>
2021-10-09 17:45:08 -0300

    actualizado ejercicio6 complementarios 2

--- Complementarios2/Ejercicio6.java
+++ Complementarios2/Ejercicio6.java
@@ -10,5 +10,46 @@ Todos los empleados están cargados en un Set (HashSet), se desea calcular el su
 (o Diccionario) donde la clave (key) es el dni y el valor (value) es el sueldo calculado.
  */
 
+import java.util.*;
+
 public class Ejercicio6 {
+    public static void main(String[] args) {
+
+        Set<Empleado> empleados = new HashSet();
+        empleados.add(new Empleado("Ale Jandro",
+                "123456", 40, 20));
+        empleados.add(new Empleado("Bat Man",
+                "234567", 4, 1000));
+        empleados.add(new Empleado("Kenny",
+                "345678", 100, 22));
+        empleados.add(new Empleado("Jason Todd",
+                "456789", 13, 4));
+
+        Map<String, Integer> sueldosEmpleados = new HashMap();
+
+        for(Empleado emp : empleados){
+            sueldosEmpleados.put(emp.DNI,
+                    Empleado.calculadorSueldo(emp.horasTrabajadas, emp.valorPorHora));
+        }
+        System.out.println("La lista de sueldos es: \n" + sueldosEmpleados);
+    }
+}
+
+class Empleado{ //esta planteado en un solo archivo, pero por buenas practicas, esta clase tendria que estar aparte
+String nombreApellido;
+String DNI;
+Integer horasTrabajadas;
+Integer valorPorHora;
+
+    public Empleado(String nombreApellido, String DNI, Integer horasTrabajadas, Integer valorPorHora) {
+        this.nombreApellido = nombreApellido;
+        this.DNI = DNI;
+        this.horasTrabajadas = horasTrabajadas;
+        this.valorPorHora = valorPorHora;
+    }
+
+    public static int calculadorSueldo(int horasTrabajadas, int valorPorHora){
+
+        return horasTrabajadas * valorPorHora;
+    }
 }
