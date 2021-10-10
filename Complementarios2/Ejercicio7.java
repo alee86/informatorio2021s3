4a48a634e9cb2a252119cbeb2b8050eed378eb92
Alejandro <alejandroeguiazabal@gmail.com>
2021-10-10 08:59:27 -0300

    completado complementarios2

--- Complementarios2/Ejercicio7.java
+++ Complementarios2/Ejercicio7.java
@@ -1,4 +1,7 @@
 package Complementarios2;
+
+import java.util.Scanner;
+
 /*
 Crear una función que dado 2 argumentos (int, siendo el primero menor al segundo),
 nos devuelva un array de Strings. Con la secuencia de números enteros de principio a final.
@@ -9,4 +12,28 @@ valor debe frenar (no se incluye en el cálculo)
 Ejemplo: (1, 5) ----> calculará valores de 1, 2, 3, 4
  */
 public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Indica el numero de inicio:");
+        int numeroInicio = scan.nextInt();
+        System.out.println("Indica el numero de fin (no se va a incluir:");
+        int numeroFin = scan.nextInt();
+        scan.close();
+
+        for (int i = numeroInicio; i < numeroFin; i++){ //Se puede reemplazar i por numeroInicio y reducir codigo
+            System.out.print(fizzBuzz(i));
+        }
+    }
+        static String fizzBuzz(int number){
+            String salida = "";
+            if ((number % 3 == 0) && (number % 2 == 0)){
+                salida = "FizBuzz ";
+            } else if (number % 3 == 0){
+                salida = "Buzz ";
+            } else if (number % 2 == 0){
+                salida = "Fizz ";
+            }
+        return salida;
+        }
 }
